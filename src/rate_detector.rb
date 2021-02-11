689876459e3414159c9c5223ecb52da09e3c3964
David Peterson <dp@vivitec.com.au>
2021-02-11 16:49:10 +1100

    Parse CSV

--- src/rate_detector.rb
+++ src/rate_detector.rb
@@ -1,3 +1,26 @@
+require 'time'
+
 class RateDetector
+
+  class Value
+    attr_reader :timestamp, :volume
+    def initialize(timestamp, volume)
+      @timestamp = Time.parse(timestamp)
+      @volume = volume
+    end
+  end
+
+  class << self
+
+    def parse(csv)
+      CSV.parse(csv, headers: true).map do |row|
+        Value.new(row["Timestamp"], row["Volume"])
+      end
+    end
+
+    def call(max_rate_change, input_csv)
+      input_array = parse(input_csv)
+    end
+  end
 end
 
